55403b4d24ae84c652198a17cb4d6d50f2845e2d
kavinkumar06 <kavinm09@gmail.com>
2017-07-12 15:53:13 +0530

    Create Digits.java

---
+++ Digits.java
@@ -0,0 +1,20 @@
+# Digits
+import java.io.*;
+import java.util.*;
+public class 
+{
+  public static void main(String args[])
+  {
+    int i=0.n;
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter a Number:");
+    n=sc.nextInt();
+    while(n>0)
+    {
+      n=n/10;
+      i++;
+    }
+    System.out.println();
+    System.out.println("Number of Digits present:"+i);
+    }
+   } 
